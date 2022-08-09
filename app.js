b046786f33d102d967015e9ff252342c0541bd93
Jason Dorr <jasondorr95@gmail.com>
2022-08-09 18:32:08 -0400

    Added color changing functions and inopperable buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector("#grid");
-const defaultColor = "#eee";
+const defaultColor = "#444";
 const defaultMode = "";
 const defaultSize = 16;
 
@@ -14,20 +14,38 @@ const getRainbow = () => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
-const changeColor = () => {
-  return (currentColor = getRainbow());
+const getGrayScale = () => {
+  return;
 };
 
-const setupGrid = (size) => {
+const changeToRainbow = () => {
+  const gridElement = document.querySelectorAll(".grid-element");
+  gridElement.forEach((element) => {
+    element.addEventListener("mouseenter", (e) => {
+      e = element.style.backgroundColor = getRainbow();
+    });
+  });
+};
+const changeToGreyScale = () => {
+  const gridElement = document.querySelectorAll(".grid-element");
+  gridElement.forEach((element) => {
+    let i = 0;
+    element.addEventListener("mouseenter", (e) => {
+      if (i < 9) {
+        i++;
+      }
+      e = element.style.backgroundColor = `rgba(0, 0, 0, .${i})`;
+    });
+  });
+};
+
+const setupGrid = (size = defaultSize) => {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-element");
-    gridElement.addEventListener("mouseenter", (e) => {
-      e = gridElement.style.backgroundColor = changeColor();
-    });
     grid.appendChild(gridElement);
   }
 };
