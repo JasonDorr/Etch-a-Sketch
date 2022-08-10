5e0c38cb30e25a166f500480e31da9bc4f2976e4
Jason Dorr <jasondorr95@gmail.com>
2022-08-10 18:31:47 -0400

    Added buttons, slider and logic to make them function.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const grid = document.querySelector("#grid");
-const defaultColor = "#444";
+const defaultColor = "#000";
 const defaultMode = "";
 const defaultSize = 16;
+
+const blackBtn = document.querySelector("#black");
+const grayBtn = document.querySelector("#gray-scale");
+const rainbowBtn = document.querySelector("#rainbow");
+const clearBtn = document.querySelector("#clear");
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+output.innerText = `${slider.value} x ${slider.value}`;
 
 let currentColor = defaultColor;
 let currentMode = defaultMode;
@@ -14,8 +22,13 @@ const getRainbow = () => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
-const getGrayScale = () => {
-  return;
+const changeToBlack = () => {
+  const gridElement = document.querySelectorAll(".grid-element");
+  gridElement.forEach((element) => {
+    element.addEventListener("mouseenter", (e) => {
+      e = element.style.backgroundColor = defaultColor;
+    });
+  });
 };
 
 const changeToRainbow = () => {
@@ -50,5 +63,40 @@ const setupGrid = (size = defaultSize) => {
   }
 };
 
-// getRainbow();
-setupGrid(currentSize);
+const clearGrid = () => {
+  const gridElement = document.querySelectorAll(".grid-element");
+  gridElement.forEach((element) => {
+    element.style.backgroundColor = "#dadada";
+  });
+};
+
+blackBtn.addEventListener("click", (e) => {
+  clearGrid();
+  return changeToBlack();
+});
+
+grayBtn.addEventListener("click", (e) => {
+  clearGrid();
+  return changeToGreyScale();
+});
+
+rainbowBtn.addEventListener("click", (e) => {
+  clearGrid();
+  return changeToRainbow();
+});
+
+clearBtn.addEventListener("click", (e) => {
+  clearGrid();
+});
+
+slider.addEventListener("input", (e) => {
+  output.innerText = `${slider.value} x ${slider.value}`;
+  const gridElement = document.querySelectorAll(".grid-element");
+  gridElement.forEach((element) => {
+    element.remove();
+  });
+  setupGrid(slider.value);
+});
+
+setupGrid(slider.value);
+changeToBlack();
